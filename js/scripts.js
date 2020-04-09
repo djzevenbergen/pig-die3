0b8cffdfbc31776e581eb3875c0fb495521a8c20
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-04-09 11:03:36 -0700

    finish algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,12 @@ Game.prototype.assignId = function () {
   return this.currentId;
 }
 
+Game.prototype.roll = function () {
+  var number = Math.floor(Math.random() * 6) + 1;
+  //console.log(number);
+  return number;
+}
+
 Game.prototype.animateDie = function (roll) {
   var rolNum = roll;
 
@@ -44,15 +50,25 @@ Game.prototype.animateDie = function (roll) {
 
 }
 
+
+
 Game.prototype.executeTurn = function (roll, turn) {
   var rollNumber = roll;
+
+
+  // turn is either 1 or 2
+  // game.players[player1, player2]
+  //     index       0          1
+  //      id         1          2 
+
+
   var index = turn - 1;
 
   $("#rollDisplay").text(rollNumber);
 
   if (rollNumber !== 1) {
     this.players[index].turnScore += rollNumber;
-    //alert(this.players[index].turnScore);
+
     $("#turnScorePlayer" + turn).text(this.players[index].turnScore);
   } else {
     this.players[index].turnScore = 0;
@@ -61,23 +77,23 @@ Game.prototype.executeTurn = function (roll, turn) {
   }
 }
 
-Game.prototype.roll = function () {
-  var number = Math.floor(Math.random() * 6) + 1;
-  //console.log(number);
-  return number;
-}
-
 Game.prototype.endTurn = function (turn) {
   var snd = new Audio("sounds/congratulations.mp3");
+
   var index = turn - 1;
   var holdPoints = this.players[index].turnScore;
+
   $("#dice").html("");
+
   this.players[index].totalScore += holdPoints;
 
   if (this.players[index].totalScore >= 20) {
     var modal = document.getElementById("myModal");
     var close = document.getElementById("close");
     snd.play();
+
+    $("#totalScorePlayer" + turn).text(this.players[index].totalScore);
+    $("#winnerName").text(this.players[index].name)
     $("#reward").show();
 
     modal.style.display = "block";
@@ -86,9 +102,6 @@ Game.prototype.endTurn = function (turn) {
       modal.style.display = "none";
       window.location.reload();
     }
-    $("#winnerName").text(this.players[index].name)
-
-    $("#totalScorePlayer" + turn).text(this.players[index].totalScore);
   }
 
   $("#totalScorePlayer" + turn).text(this.players[index].totalScore);
@@ -111,9 +124,9 @@ Game.prototype.changeTurn = function (turn) {
 }
 
 //only when player 1 or player 2 score reaches 100 after endTurn is executed
-Game.prototype.gameOver = function () {
+// Game.prototype.gameOver = function () {
 
-}
+// }
 
 // user interface
 $(document).ready(function () {
@@ -130,7 +143,6 @@ $(document).ready(function () {
     var pTwo = new Player(pTwoName);
     var game = new Game();
 
-
     game.addPlayer(pOne);
     game.addPlayer(pTwo);
 
@@ -141,17 +153,11 @@ $(document).ready(function () {
     $("#playerTwoNameDisplay").text(pTwo.name);
     $("#turnDisplay").text(pOne.name);
 
-    console.log(pOne.name);
-    console.log(pTwo.name);
-    console.log(game);
-
     $("#rollButton").mousedown(function () {
       $("#dice").html('<p>########<p/><p>########<p/><p>########<p/>');
       //$("#dice").html('<div class="test" contenteditable><p class="smaller">########<p/><p class="smaller">########</p></div><svg xmlns="http://www.w3.org/2000/svg" version="1.1"><defs><filter id="squiggly-0"><feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="0"/><feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="6" /></filter><filter id="squiggly-1"><feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="1"/><feDisplacementMap in="SourceGraphic" in2="noise" scale="8" /></filter><filter id="squiggly-2"><feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="2"/><feDisplacementMap in="SourceGraphic" in2="noise" scale="6" /></filter><filter id="squiggly-3"><feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="3"/><feDisplacementMap in="SourceGraphic" in2="noise" scale="8" /></filter><filter id="squiggly-4"><feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="4"/><feDisplacementMap in="SourceGraphic" in2="noise" scale="6" /></filter></defs></svg>');
-
-
-
     });
+
     $("#rollButton").mouseup(function () {
       var roll = game.roll();
       var turn = game.playerTurn;
